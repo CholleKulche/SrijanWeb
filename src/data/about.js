828a40c25b41bfee50540ef0e112bea8b028635f
CholleKulche <jyotin.singh.ug23@nsut.ac.in>
2025-07-15 10:14:18 +0530

    Fixed some bug

--- src/data/about.js
+++ src/data/about.js
@@ -1,61 +1,61 @@
 export const projectLeads = [
   {
-    img: "/data/Images/assets/Images/KirtanGupta.png",
+    img: "/assets/Images/assets/Images/KirtanGupta.png",
     name: "Kirtan Gupta",
   },
   {
-    img: "/data/Images/assets/Images/GarvMittal.png",
+    img: "/assets/Images/assets/Images/GarvMittal.png",
     name: "Garv Mittal",
   },
 ];
 export const deptLeads = [
   {
-    img: "/data/Images/assets/Images/DishaKashyap.png",
+    img: "/assets/Images/assets/Images/DishaKashyap.png",
     name: "Disha Kashyap",
     post: "Research and Development",
   },
   {
-    img: "/data/Images/assets/Images/VanshBhardwaj.png",
+    img: "/assets/Images/assets/Images/VanshBhardwaj.png",
     name: "Vansh Bhardwaj",
     post: "Research and Development",
   },
   {
-    img: "/data/Images/assets/Images/VedanshBhaiya.png",
+    img: "/assets/Images/assets/Images/VedanshBhaiya.png",
     name: "Urvi Ghai",
     post: "Research and Development / Media",
   },
   {
-    img: "/data/Images/assets/Images/YashmitGupta.png",
+    img: "/assets/Images/assets/Images/YashmitGupta.png",
     name: "Yashmit Gupta",
     post: "Media",
   },
   {
-    img: "/data/Images/assets/Images/Vedansh.png",
+    img: "/assets/Images/assets/Images/Vedansh.png",
     name: "Vedansh",
     post: "Media",
   },
   {
-    img: "/data/Images/assets/Images/MananBhateja.png",
+    img: "/assets/Images/assets/Images/MananBhateja.png",
     name: "Manan Bhateja",
     post: "Media",
   },
   {
-    img: "/data/Images/assets/Images/JyotinThakur.png",
+    img: "/assets/Images/assets/Images/JyotinThakur.png",
     name: "Jyotin Thakur",
     post: "Tech",
   },
   {
-    img: "/data/Images/assets/Images/SuhaniGarg.png",
+    img: "/assets/Images/assets/Images/SuhaniGarg.png",
     name: "Suhani Garg",
     post: "Tech",
   },
   {
-    img: "/data/Images/assets/Images/Ritveek.png",
+    img: "/assets/Images/assets/Images/Ritveek.png",
     name: "Ritveek",
     post: "Business Outreach",
   },
   {
-    img: "/data/Images/assets/Images/SatvikGupta.png",
+    img: "/assets/Images/assets/Images/SatvikGupta.png",
     name: "Satvik",
     post: "Business Outreach",
   },
--- src/data/home.js
+++ src/data/home.js
@@ -1,43 +1,43 @@
 export const steps = [
   {
-    img: "/data/Images/assets/Images/Paper Creation.png",
+    img: "/assets/Images/assets/Images/Paper Creation.png",
     title: "Paper Creation",
     content: "Recycled materials are processed into paper pulp",
   },
   {
-    img: "/data/Images/assets/Images/Seed Embedding.png",
+    img: "/assets/Images/assets/Images/Seed Embedding.png",
     title: "Seed Embedding",
     content: "High-quality seeds are carefully embedded",
   },
   {
-    img: "/data/Images/assets/Images/Usage.png",
+    img: "/assets/Images/assets/Images/Usage.png",
     title: "Usage",
     content: "Use the product as intended",
   },
   {
-    img: "/data/Images/assets/Images/Planting.png",
+    img: "/assets/Images/assets/Images/Planting.png",
     title: "Planting",
     content: "Plant and watch it grow into beautiful plants",
   },
 ];
 export const products = [
   {
-    img: "/data/Images/assets/Images/Corporate Gift Sets.png",
+    img: "/assets/Images/assets/Images/Corporate Gift Sets.png",
     product: "Corporate Gift Sets",
     content: "Track time while planning for a greener future",
   },
   {
-    img: "/data/Images/assets/Images/Ecofriendly Calanders.png",
+    img: "/assets/Images/assets/Images/Ecofriendly Calanders.png",
     product: "Ecofriendly Calenders",
     content: "Sustainable gifting solutions for businesses",
   },
   {
-    img: "/data/Images/assets/Images/PlantableJournal.png",
+    img: "/assets/Images/assets/Images/PlantableJournal.png",
     product: "Plantable Journals",
     content: "Write memoirs that blossom into flowers",
   },
   {
-    img: "/data/Images/assets/Images/Seed Paper Business Cards.png",
+    img: "/assets/Images/assets/Images/Seed Paper Business Cards.png",
     product: "Seed Paper Business Cards",
     content: "Make a lasting impression with cards that grow into plants",
   },
